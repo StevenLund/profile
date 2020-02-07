cb2c6e5755c8bad262dd955d8dd164c07b142660
Steven Lund <steve.a.lund@gmail.com>
2020-02-06 23:58:06 -0700

    testing slider-images

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : '/img/slider-images/classroom.jpg', title : '<div class="slide-content">Steven Lund</div><div class="slide-content-sub">Full-Stack Development, Mechanical Engineering</div>', thumb : '', url : ''},
+											{image : '/img/slider-images/Ryan_Chong.jpg', title : '<div class="slide-content">Steven Lund</div><div class="slide-content-sub">Full-Stack Development, Mechanical Engineering</div>', thumb : '', url : ''},
 											{image : '/img/slider-images/colorado.jpg', title : '<div class="slide-content">Steven Lund</div><div class="slide-content-sub">Full-Stack Development, Mechanical Engineering</div>', thumb : '', url : ''},
 											{image : '/img/slider-images/band.jpg', title : '<div class="slide-content">Steven Lund</div><div class="slide-content-sub">Full-Stack Development, Mechanical Engineering</div>', thumb : '', url : ''},
 											{image : '/img/slider-images/beach.jpg', title : '<div class="slide-content">Steven Lund</div><div class="slide-content-sub">Full-Stack Development, Mechanical Engineering</div>', thumb : '', url : ''}
